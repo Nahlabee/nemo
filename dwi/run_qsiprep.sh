4efd1a6df1b65f3c911e54f93c62a031cf5cf294
Lucile <lucile.brun@gmx.com>
2025-11-24 16:30:22 +0100

    test qsiprep

--- dwi/run_qsiprep.sh
+++ dwi/run_qsiprep.sh
@@ -11,4 +11,4 @@ CONFIG_FILE="../config.py"
 eval $(python3 -c 'import config; config.print_paths()')
 
 # Execute Singularity container
-apptainer run --nv -B $DATA_BIDS_DIR:/data,$DERIVATIVES_BIDS_DIR:/out,$FREESURFER_LICENSE/license.txt:/opt/freesurfer/license.txt $QSIPREP_CONTAINER /data /out participant --participant-label sub-1054001 --session-id ses-01 --skip-bids-validation True -v -w /out/temp_wf_qsiprep --fs-license-file /opt/freesurfer/license.txt --output-resolution 1.2
+apptainer run --nv -B $DATA_BIDS_DIR:/data,$DERIVATIVES_BIDS_DIR:/out,$FREESURFER_LICENSE/license.txt:/opt/freesurfer/license.txt $QSIPREP_CONTAINER /data /out participant --participant-label sub-1054001 --session-id ses-01 --skip-bids-validation -v -w /out/temp_wf_qsiprep --fs-license-file /opt/freesurfer/license.txt --output-resolution 1.2
