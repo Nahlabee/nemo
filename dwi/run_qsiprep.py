1e84eadeef76053b07447df878e05acec7b561d7
Lucile <lucile.brun@gmx.com>
2025-12-03 16:42:31 +0100

    refactor run qsiprep

---
+++ dwi/run_qsiprep.py
@@ -0,0 +1,170 @@
+import json
+import os
+import shutil
+from datetime import datetime
+from types import SimpleNamespace
+from pathlib import Path
+import sys
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+import utils
+
+
+def generate_slurm_script(args, subject, session, path_to_script, job_ids=None):
+    """
+    Generate the SLURM script for QSIprep processing.
+
+    Parameters
+    ----------
+    args : Namespace
+        Configuration arguments containing parameters for SLURM and QSIprep.
+    subject : str
+        Subject identifier.
+    session : str
+        Session identifier.
+    path_to_script : str
+        Path where the SLURM script will be saved.
+    job_ids : list, optional
+        List of SLURM job IDs to set as dependencies (default is None).
+    """
+    if job_ids is None:
+        job_ids = []
+
+    header = (
+        f'#!/bin/bash\n'
+        f'#SBATCH -J qsiprep_{subject}_{session}\n'
+        f'#SBATCH -p {args.partition}\n'
+        f'#SBATCH --gpus-per-node={args.gpu_per_node}\n'
+        f'#SBATCH --nodes=1\n'
+        f'#SBATCH --mem={args.requested_mem}gb\n'
+        f'#SBATCH -t {args.requested_time}:00:00\n'
+        f'#SBATCH -e {args.derivatives}/qsiprep/stdout/%x_job-%j.err\n'
+        f'#SBATCH -o {args.derivatives}/qsiprep/stdout/%x_job-%j.out\n'
+    )
+
+    if job_ids:
+        header += (
+            f'#SBATCH --dependency=afterok:{":".join(job_ids)}\n'
+        )
+
+    if args.email:
+        header += (
+            f'#SBATCH --mail-type=BEGIN,END\n'
+            f'#SBATCH --mail-user={args.email}\n'
+        )
+
+    if args.account:
+        header += f'#SBATCH --account={args.account}\n'
+
+    module_export = (
+        f'\nmodule purge\n'
+        f'module load userspace/all\n'
+        f'module load singularity\n'
+    )
+
+    # Define the Singularity command for running QSIprep
+    # Note: Temporary binding to a local FreeSurfer version is included
+    # todo: After PR accepted and new container built, remove bound to local freesurfer 7.4.1 and env variable
+    singularity_command = (
+        f'\napptainer run \\\n'
+        f'    --cleanenv \\\n'
+        f'    -B {args.input_dir}:/data \\\n'
+        f'    -B {args.derivatives}/qsiprep:/out \\\n'
+        f'    -B {args.freesurfer_license}:/license \\\n'
+        f'    -B {args.config_eddy}:/config/eddy-config.json \\\n'
+        f'    -B {args.config_qsiprep}:/config/config-file.toml \\\n'
+        f'    -B /scratch/lhashimoto/freesurfer-7.4.1/usr/local/freesurfer:/opt/freesurfer:ro \\\n'
+        f'    --env FREESURFER_HOME=/opt/freesurfer \\\n'
+        f'    {args.qsiprep_container} /data /out participant \\\n'
+        f'    --participant-label {subject} --session-id {session} \\\n'
+        f'    --skip-bids-validation -v -w /out/temp_qsiprep \\\n'
+        f'    --fs-license-file /opt/freesurfer/license.txt \\\n'
+        f'    --eddy-config /config/eddy_params.json \\\n'
+        f'    --config-file /config/config-file.toml \\\n'
+        f'    --output-resolution {args.output_resolution}\n'
+    )
+
+    # Add permissions for shared ownership of the output directory
+    ownership_sharing = f'\nchmod -Rf 771 {args.derivatives}/qsiprep\n'
+
+    # Write the complete SLURM script to the specified file
+    with open(path_to_script, 'w') as f:
+        f.write(header + module_export + singularity_command + ownership_sharing)
+
+
+def run_qsiprep(args, subject, session, job_ids=None):
+    """
+    Run the QSIprep for a given subject and session.
+
+    Parameters
+    ----------
+    job_ids
+    args : Namespace
+        Configuration arguments.
+    subject : str
+        Subject identifier.
+    session : str
+        Session identifier.
+
+    Returns
+    -------
+    str or None
+        SLURM job ID if the job is submitted successfully, None otherwise.
+    """
+
+    # QSIprep manages already processed subjects.
+    # No need to remove existing folder or skip subjects.
+    # Required files are checked inside the process.
+
+    # Create output (derivatives) directories
+    if job_ids is None:
+        job_ids = []
+    os.makedirs(f"{args.derivatives}/qsiprep", exist_ok=True)
+    os.makedirs(f"{args.derivatives}/qsiprep/stdout", exist_ok=True)
+    os.makedirs(f"{args.derivatives}/qsiprep/scripts", exist_ok=True)
+
+    path_to_script = f"{args.derivatives}/qsiprep/scripts/{subject}_{session}_qsiprep.slurm"
+    generate_slurm_script(args, subject, session, path_to_script, job_ids)
+
+    cmd = f"sbatch {path_to_script}"
+    print(f"Submitting job: {cmd}")
+    job_id = utils.submit_job(cmd)
+    return job_id
+
+
+def main(config_file=None):
+    """
+    Main function to execute FreeSurfer processing for a list of subjects and sessions.
+    """
+    # Load configuration
+    if not config_file:
+        config_file = f"{Path(__file__).parent.parent}/config/config.json"
+    config = utils.load_config(config_file)
+    args = SimpleNamespace()
+    sub_keys = ['common', 'qsiprep']
+    for sub_key in sub_keys:
+        step_config = config.get(sub_key, {})
+        for key, value in step_config.items():
+            setattr(args, key, value)
+
+    # Check dataset directory
+    if not os.path.exists(args.input_dir):
+        print("Dataset directory does not exist.")
+        return 0
+
+    # Loop over subjects and sessions
+    subjects = utils.get_subjects(args.input_dir, args.subjects)
+    for subject in subjects:
+        sessions = utils.get_sessions(args.input_dir, subject, args.sessions)
+        for session in sessions:
+            run_qsiprep(args, subject, session)
+
+    # Save config in json
+    config = vars(args)
+    filename = f"{args.derivatives}/config_{datetime.now().strftime('%Y%m%d-%H%M%S')}.json"
+    with open(filename, "w") as f:
+        json.dump(config, f, indent=4)
+
+
+if __name__ == "__main__":
+    main()
+
--- dwi/workflow_diffusion.py
+++ dwi/workflow_diffusion.py
@@ -3,44 +3,11 @@ import json
 import sys
 import subprocess
 from pathlib import Path
-
+import utils
 sys.path.append(str(Path(__file__).parent.parent))
-from utils import load_config
 
 
-def submit_qsiprep_job(cmd):
-    """
-    Soumet un job SLURM pour qsiprep et retourne l'ID du job.
-
-    Parameters
-    ----------
-    script_path : str
-        Chemin vers le script SLURM à soumettre.
-
-    Returns
-    -------
-    job_id : str
-        ID du job SLURM soumis.
-    """
-    try:
-        # Exécute la commande sbatch et capture la sortie
-        result = subprocess.run(cmd, shell=True, check=True, text=True, capture_output=True)
-        output = result.stdout.strip()
-
-        # Analyse la sortie pour extraire l'ID du job
-        if output.startswith("Submitted batch job"):
-            job_id = output.split()[-1]
-            print(f"Job SLURM soumis avec succès : ID {job_id}")
-            return job_id
-        else:
-            print("Impossible de récupérer l'ID du job SLURM.")
-            return None
-    except subprocess.CalledProcessError as e:
-        print(f"Erreur lors de la soumission du job SLURM : {e}")
-        return None
-
-
-def do_qsiprep(args):
+def run_qsiprep(args, freesurfer_job_ids=None):
     """
 
     Parameters
@@ -96,73 +63,71 @@ def do_qsiprep(args):
                 session = 'ses-' + session
 
             print(subject, ' - ', session)
-            path_to_output = os.path.join(args.output_dir, f"{subject}/{session}")
 
             # QSIprep manages already processed subjects.
             # No need to remove existing folder or skip subjects.
 
             # Write and launch slurm commands
-            header = \
-                ('#!/bin/bash\n'
-                 '#SBATCH -J qsiprep_{0}_{1}\n'
-                 '#SBATCH -p {2}\n'
-                 '#SBATCH --gpus-per-node={3}\n'
-                 '#SBATCH --nodes=1\n'
-                 '#SBATCH --mem={4}gb\n'
-                 '#SBATCH -t {5}:00:00\n'
-                 '#SBATCH -e {6}/stdout/%x_job-%j.err\n'
-                 '#SBATCH -o {6}/stdout/%x_job-%j.out\n').format(subject, session,
-                                                                 args.partition,
-                                                                 args.gpu_per_node,
-                                                                 args.requested_mem,
-                                                                 args.requested_time,
-                                                                 args.output_dir)
+            header = (
+                f'#!/bin/bash\n'
+                f'#SBATCH -J qsiprep_{subject}_{session}\n'
+                f'#SBATCH -p {args.partition}\n'
+                f'#SBATCH --gpus-per-node={args.gpu_per_node}\n'
+                f'#SBATCH --nodes=1\n'
+                f'#SBATCH --mem={args.requested_mem}gb\n'
+                f'#SBATCH -t {args.requested_time}:00:00\n'
+                f'#SBATCH -e {args.output_dir}/stdout/%x_job-%j.err\n'
+                f'#SBATCH -o {args.output_dir}/stdout/%x_job-%j.out\n'
+            )
+
+            if freesurfer_job_ids:
+                header += (
+                    f'#SBATCH --dependency=afterok:{":".join(freesurfer_job_ids[subject][session])}\n'
+                )
 
             if args.email:
-                header += \
-                    ('#SBATCH --mail-type=BEGIN,END\n'
-                     '#SBATCH --mail-user={}\n').format(args.email)
+                header += (
+                    f'#SBATCH --mail-type=BEGIN,END\n'
+                    f'#SBATCH --mail-user={args.email}\n'
+                )
 
             if args.account:
-                header += \
-                    '#SBATCH --account={}\n'.format(args.account)
+                header += (
+                    f'#SBATCH --account={args.account}\n'
+                )
 
-            module_export = \
-                ('\n'
-                 'module purge\n'
-                 'module load userspace/all\n'
-                 'module load singularity\n').format(args.input_dir)
+            module_export = (
+                f'\n'
+                f'module purge\n'
+                f'module load userspace/all\n'
+                f'module load singularity\n'
+            )
 
             # todo: After PR accepted and new container built, remove bound to local freesurfer 7.4.1 and env variable
-            singularity_command = \
-                ('\n'
-                 'apptainer run \\\n'
-                 '    --nv --cleanenv \\\n'
-                 '    -B {0}:/data \\\n'
-                 '    -B {1}:/out \\\n'
-                 '    -B /scratch/lhashimoto/freesurfer-7.4.1/usr/local/freesurfer:/opt/freesurfer:ro \\\n'
-                 '    -B {2}/license.txt:/opt/freesurfer/license.txt \\\n'
-                 '    -B {6}:/config/eddy-config.json \\\n'
-                 '    -B {7}:/config/config-file.toml \\\n'
-                 '    --env FREESURFER_HOME=/opt/freesurfer \\\n'
-                 '    {3} /data /out participant \\\n'
-                 '    --participant-label {4} --session-id {5} \\\n'
-                 '    --skip-bids-validation -v -w /out/temp_qsiprep \\\n'
-                 '    --fs-license-file /opt/freesurfer/license.txt \\\n'
-                 '    --eddy-config /config/eddy_params.json \\\n'
-                 '    --config-file /config/config-file.toml \\\n'
-                 '    --output-resolution {8}\n').format(args.input_dir,
-                                                         args.output_dir,
-                                                         args.freesurfer_license,
-                                                         args.qsiprep_container,
-                                                         subject, session,
-                                                         args.config_eddy,
-                                                         args.config_qsiprep,
-                                                         args.output_resolution)
+            singularity_command = (
+                f'\n'
+                f'apptainer run \\\n'
+                f'    --nv --cleanenv \\\n'
+                f'    -B {args.input_dir}:/data \\\n'
+                f'    -B {args.output_dir}:/out \\\n'
+                f'    -B /scratch/lhashimoto/freesurfer-7.4.1/usr/local/freesurfer:/opt/freesurfer:ro \\\n'
+                f'    -B {args.freesurfer_license}/license.txt:/opt/freesurfer/license.txt \\\n'
+                f'    -B {args.config_eddy}:/config/eddy-config.json \\\n'
+                f'    -B {args.config_qsiprep}:/config/config-file.toml \\\n'
+                f'    --env FREESURFER_HOME=/opt/freesurfer \\\n'
+                f'    {args.qsiprep_container} /data /out participant \\\n'
+                f'    --participant-label {subject} --session-id {session} \\\n'
+                f'    --skip-bids-validation -v -w /out/temp_qsiprep \\\n'
+                f'    --fs-license-file /opt/freesurfer/license.txt \\\n'
+                f'    --eddy-config /config/eddy_params.json \\\n'
+                f'    --config-file /config/config-file.toml \\\n'
+                f'    --output-resolution {args.output_resolution}\n'
+            )
 
-            ownership_sharing = \
-                ('\n'
-                 'chmod -Rf 771 {0}\n').format(args.output_dir)
+            ownership_sharing = (
+                f'\n'
+                f'chmod -Rf 771 {args.output_dir}\n'
+            )
 
             path_to_script = f'{args.output_dir}/scripts/{subject}_{session}_qsiprep.slurm'
             with open(path_to_script, 'w') as f:
@@ -170,8 +135,8 @@ def do_qsiprep(args):
 
             # launch slurm script
             cmd = f"sbatch {path_to_script}"
-            print(cmd)
-            job_id = submit_qsiprep_job(cmd)
+            print(f"Submitting QSIprep job: {cmd}")
+            job_id = utils.submit_job(cmd)
             job_ids[subject][session] = job_id
             # subprocess.run(cmd, shell=True, check=True)
 
@@ -230,7 +195,7 @@ def check_preprocessing_completion(args, subject, session):
     return False
 
 
-def do_qsirecon(args, qsiprep_job_ids=None):
+def run_qsirecon(args, qsiprep_job_ids=None):
     """
     Executes the postprocessing step with QSIrecon after the preprocessing step with QSIprep has successfully completed.
 
@@ -251,10 +216,12 @@ def do_qsirecon(args, qsiprep_job_ids=None):
         scripts to execute the postprocessing step with QSIrecon.
     """
 
+    job_ids = {}
+
     # Check dataset directory
     if not os.path.exists(args.input_dir):
         print("Dataset directory does not exist.")
-        return
+        return 0
 
     # Create output (derivatives) directories
     if not os.path.exists(args.output_dir):
@@ -275,6 +242,8 @@ def do_qsirecon(args, qsiprep_job_ids=None):
         # Add sub prefix if not given by the user
         if not subject.startswith('sub-'):
             subject = 'sub-' + subject
+
+        job_ids[subject] = {}
 
         # Define sessions list
         path_to_subject = os.path.join(args.input_dir, subject)
@@ -299,7 +268,7 @@ def do_qsirecon(args, qsiprep_job_ids=None):
 
             # Check input files
             if not check_preprocessing_completion(args, subject, session):
-                return
+                return 0
 
             # Write and launch slurm commands
             header = (
@@ -316,7 +285,7 @@ def do_qsirecon(args, qsiprep_job_ids=None):
             )
             if qsiprep_job_ids:
                 header += (
-                    f'#SBATCH --dependency=afterok:{":".join(qsiprep_job_ids)}\n'
+                    f'#SBATCH --dependency=afterok:{":".join(qsiprep_job_ids[subject][session])}\n'
                 )
 
             if args.email:
@@ -364,9 +333,12 @@ def do_qsirecon(args, qsiprep_job_ids=None):
 
             # launch slurm script
             cmd = f"sbatch {path_to_script}"
-            print(cmd)
-            a = os.system(cmd)
+            print(f"Submitting QSIrecon job: {cmd}")
+            job_id = utils.submit_job(cmd)
+            job_ids[subject][session] = job_id
             # subprocess.run(cmd, shell=True, check=True)
+
+    return job_ids
 
 
 def main_old(raw_args=None):
@@ -442,7 +414,7 @@ def main_old(raw_args=None):
         json.dump(config, f, indent=4)
 
     # todo: check gpu available?
-    do_qsiprep(args)
+    run_qsiprep(args)
 
 
 def main():
@@ -455,7 +427,7 @@ def main():
     # Read arguments from config file.
     args = SimpleNamespace()
     general_config_file = f"{Path(__file__).parent.parent}/config.json"
-    config = load_config(general_config_file)
+    config = utils.load_config(general_config_file)
     sub_keys = ['common', 'slurm', 'qsiprep']
     for sub_key in sub_keys:
         workflow_config = config.get(sub_key, {})
@@ -470,8 +442,7 @@ def main():
     with open(os.path.join(args.output_dir, 'config.json'), "w") as f:
         json.dump(config, f, indent=4)
 
-    # todo: check gpu available?
-    do_qsiprep(args)
+    run_qsiprep(args)
 
     ###########################################################################
     #                       QSIrecon
@@ -480,7 +451,7 @@ def main():
     # Read arguments from config file.
     args = SimpleNamespace()
     general_config_file = f"{Path(__file__).parent.parent}/config.json"
-    config = load_config(general_config_file)
+    config = utils.load_config(general_config_file)
     sub_keys = ['common', 'slurm', 'qsirecon']
     for sub_key in sub_keys:
         workflow_config = config.get(sub_key, {})
@@ -495,7 +466,7 @@ def main():
     with open(os.path.join(args.output_dir, 'config.json'), "w") as f:
         json.dump(config, f, indent=4)
 
-    do_qsirecon(args)
+    run_qsirecon(args)
 
 
 if __name__ == '__main__':
