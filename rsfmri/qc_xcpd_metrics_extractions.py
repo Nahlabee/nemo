af332b3fd15b37dbbb1ec9bef991e44b40b4c58c
Lucile <lucile.brun@gmx.com>
2026-01-01 15:29:24 +0100

    test qc xcpd

--- rsfmri/qc_xcpd_metrics_extractions.py
+++ rsfmri/qc_xcpd_metrics_extractions.py
@@ -2,6 +2,9 @@
 import json
 import os
 import pandas as pd
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).resolve().parent.parent))
 import utils
 
 # # ------------------------------
